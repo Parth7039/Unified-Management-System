129069790de6b2d6205f10a8fbaac1f52bcdfb40
Parth7039 <p.k.bhamare07@gmail.com>
2024-08-10 22:34:07 +0530

    first commit

--- lib/Dashboard/dashboard.dart
+++ lib/Dashboard/dashboard.dart
@@ -15,72 +15,72 @@ class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color(0xFF8697C4),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Row(
           children: [
             CustomContainer(),
             Expanded(
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Container(
-                        height: 120,
-                        width: 250,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(
-                            color: Colors.black,
-                            width: 2
-                          )
-                        ),
-                      ),
-                      Container(
-                        height: 120,
-                        width: 250,
-                        decoration: BoxDecoration(
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          height: 120,
+                          width: 250,
+                          decoration: BoxDecoration(
                             color: Colors.white,
                             borderRadius: BorderRadius.circular(10),
                             border: Border.all(
-                                color: Colors.black,
-                                width: 2
+                              color: Colors.black,
+                              width: 2
                             )
+                          ),
                         ),
-                      ),
-                      Container(
-                        height: 120,
-                        width: 250,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(
-                                color: Colors.black,
-                                width: 2
-                            )
+                        Container(
+                          height: 120,
+                          width: 250,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                  color: Colors.black,
+                                  width: 2
+                              )
+                          ),
                         ),
-                      ),
-                      Container(
-                        height: 120,
-                        width: 250,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(
-                                color: Colors.black,
-                                width: 2
-                            )
+                        Container(
+                          height: 120,
+                          width: 250,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                  color: Colors.black,
+                                  width: 2
+                              )
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 15,),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8.0, left: 8),
-                    child: Row(
+                        Container(
+                          height: 120,
+                          width: 250,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                  color: Colors.black,
+                                  width: 2
+                              )
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 15,),
+                    Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Container(
@@ -98,16 +98,32 @@ class _DashboardPageState extends State<DashboardPage> {
                         ),
                         Container(
                           height: 400,
-                          width: 500,
+                          width: 1000,
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10),
                             border: Border.all(width: 1,color: Colors.black)
                           ),
+                          child: Center(
+                            child: Text('This is a graph'),
+                          ),
                         )
                       ],
                     ),
-                  )
-                ],
+                    SizedBox(height: 15,),
+                    Row(
+                      children: [
+                        Container(
+                          width: 300,
+                          height: 400,
+                          decoration: BoxDecoration(
+                            border: Border.all(width: 2,color: Colors.black),
+                            borderRadius: BorderRadius.circular(10)
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             )
           ],
