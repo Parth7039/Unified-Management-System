258ceda154009d677e75f4796b350f30fe8d462c
Parth7039 <p.k.bhamare07@gmail.com>
2024-08-11 23:25:21 +0530

    first commit

--- lib/Feature/Dashboard/dashboard.dart
+++ lib/Feature/Dashboard/dashboard.dart
@@ -99,6 +99,62 @@ class _DashboardPageState extends State<DashboardPage> {
                   )
                 ),
               ],
+            ),
+            SizedBox(height: 20,),
+            Row(
+              children: [
+                Card(
+                  elevation: 5,
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    height: 450,
+                    width: 380,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(13)
+                    ),
+                    child: ListView(
+                      children: [
+                        Center(child: Text('Ongoing Projects',style: TextStyle(fontWeight: FontWeight.bold),)),
+                        SizedBox(height: 15,),
+                        ListTile(
+                          tileColor: Colors.black,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                          title: Text('ABC site',style: TextStyle(color: Colors.white),),
+                          subtitle: Text('Mahalaxmi Road'),
+                        ),
+                        SizedBox(height: 15,),
+                        ListTile(
+                          tileColor: Colors.black,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                          title: Text('XYZ site',style: TextStyle(color: Colors.white),),
+                          subtitle: Text('M.G marg'),
+                        ),
+                        SizedBox(height: 15,),
+                        ListTile(
+                          tileColor: Colors.black,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                          title: Text('PQR site',style: TextStyle(color: Colors.white),),
+                          subtitle: Text('College Road'),
+                        ),
+                        SizedBox(height: 15,),
+                        ListTile(
+                          tileColor: Colors.black,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                          title: Text('LMN site',style: TextStyle(color: Colors.white),),
+                          subtitle: Text('Grant Road'),
+                        ),
+                        SizedBox(height: 15,),
+                        ListTile(
+                          tileColor: Colors.black,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                          title: Text('LMN site',style: TextStyle(color: Colors.white),),
+                          subtitle: Text('Grant Road'),
+                        ),
+                      ],
+                    )
+                  ),
+                )
+              ],
             )
           ],
         ),
