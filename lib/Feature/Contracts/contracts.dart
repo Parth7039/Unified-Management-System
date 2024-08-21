aa33c07a6a1e02269db7d9af3ec318b9943381d9
Parth7039 <p.k.bhamare07@gmail.com>
2024-08-21 22:32:46 +0530

    first commit

--- lib/Feature/Contracts/contracts.dart
+++ lib/Feature/Contracts/contracts.dart
@@ -9,6 +9,10 @@ class ContractsPage extends StatefulWidget {
 }
 
 class _ContractsPageState extends State<ContractsPage> {
+
+  TextEditingController _startdateController = TextEditingController();
+  TextEditingController _enddateController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +20,14 @@ class _ContractsPageState extends State<ContractsPage> {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.white,
         child: Icon(Icons.add, color: Colors.black),
-        onPressed: () {},
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return alertDialog();
+            },
+          );
+        },
       ),
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -30,64 +41,71 @@ class _ContractsPageState extends State<ContractsPage> {
           },
         ),
       ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              _buildContractCard('Active Contracts', '5'),
-              _buildContractCard('Inactive Contracts', '5'),
-              _buildContractCard('Completed Contracts', '10'),
-              _buildContractCard('Total Contracts', '19'),
-            ],
-          ),
-          SizedBox(height: 15),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 700,
-              width: 900,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.circular(15),
-                border: Border.all(color: Colors.black),
-              ),
-              child: Column(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextField(
-                      cursorColor: Colors.black,
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        hintText: 'Search contracts..',
-                        prefixIcon: Icon(Icons.search, color: Colors.black),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black),
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                          borderSide: BorderSide(color: Colors.black),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: ListView(
-                      children: [
-                        _buildContractListTile('Contract 1', 'Details about contract 1', Colors.red),
-                        _buildContractListTile('Contract 2', 'Details about contract 2', Colors.green),
-                        _buildContractListTile('Contract 3', 'Details about contract 3', Colors.red),
-                      ],
-                    ),
-                  ),
+                  _buildContractCard('Active Contracts', '5'),
+                  _buildContractCard('Inactive Contracts', '5'),
+                  _buildContractCard('Completed Contracts', '10'),
+                  _buildContractCard('Total Contracts', '19'),
                 ],
               ),
             ),
-          ),
-        ],
+            SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        cursorColor: Colors.black,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          hintText: 'Search contracts..',
+                          prefixIcon: Icon(Icons.search, color: Colors.black),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 600,
+                      child: ListView(
+                        children: [
+                          _buildContractListTile(
+                              'Contract 1', 'Details about contract 1', Colors.red),
+                          _buildContractListTile(
+                              'Contract 2', 'Details about contract 2', Colors.green),
+                          _buildContractListTile(
+                              'Contract 3', 'Details about contract 3', Colors.red),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -97,7 +115,7 @@ class _ContractsPageState extends State<ContractsPage> {
       elevation: 5,
       child: Container(
         height: 120,
-        width: 300,
+        width: 190,
         decoration: BoxDecoration(
           border: Border.all(color: Colors.black, width: 1),
           borderRadius: BorderRadius.circular(13),
@@ -127,18 +145,166 @@ class _ContractsPageState extends State<ContractsPage> {
       tileColor: Colors.deepOrange,
       title: Text(
         title,
-        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,fontSize: 25),
+        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
       ),
       subtitle: Text(
         subtitle,
         style: TextStyle(color: Colors.white70),
       ),
       trailing: Icon(
-        Icons.circle,color: color,
+        Icons.circle,
+        color: color,
       ),
       onTap: () {
         // Handle tile tap
       },
     );
   }
+
+  Widget alertDialog() {
+    return AlertDialog(
+      backgroundColor: Colors.white,
+      title: Center(child: Text('Add New Contract')),
+      content: Container(
+        height: 650,
+        width: 900,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: [
+                customTextField('Contract Name', 'Enter name here..'),
+                SizedBox(height: 20),
+                customTextField('Contract Details', 'Enter details here..'),
+                SizedBox(height: 20,),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      _selectStartDate(context);
+                    },
+                    child: AbsorbPointer(
+                      child: Container(
+                        width: 420,
+                        child: TextField(
+                          controller: _startdateController,
+                          readOnly: true,
+                          decoration: InputDecoration(
+                            labelText: 'Start Date',
+                            prefixIcon: Icon(Icons.calendar_today),
+                            enabledBorder: OutlineInputBorder(),
+                            focusedBorder: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                customTextField('Contract duration', 'Enter duration here..'),
+                SizedBox(height: 20),
+                customTextField('Additional terms', 'Enter additional terms here..'),
+                SizedBox(height: 20,),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      _selectEndDate(context);
+                    },
+                    child: AbsorbPointer(
+                      child: Container(
+                        width: 420,
+                        child: TextField(
+                          controller: _enddateController,
+                          readOnly: true,
+                          decoration: InputDecoration(
+                            labelText: 'End Date',
+                            prefixIcon: Icon(Icons.calendar_today),
+                            enabledBorder: OutlineInputBorder(),
+                            focusedBorder: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        TextButton(
+          child: Text('Cancel'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        TextButton(
+          child: Text('Add'),
+          onPressed: () {
+            // Add contract logic
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget customTextField(String labelText, String hintText) {
+    return Container(
+      width: 420,  // Set the fixed width here
+      child: TextField(
+        cursorColor: Colors.black,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          hintText: hintText,
+          labelText: labelText,
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide(color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _selectStartDate(BuildContext context) async {
+    final DateTime? picked1 = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2400),
+    );
+
+    if (picked1 != null) {
+      setState(() {
+        _startdateController.text = picked1.toString().split(" ")[0];
+      });
+    }
+  }
+
+  Future<void> _selectEndDate(BuildContext context) async {
+    final DateTime? picked2 = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2400),
+    );
+
+    if (picked2 != null) {
+      setState(() {
+        _enddateController.text = picked2.toString().split(" ")[0];
+      });
+    }
+  }
+
 }
